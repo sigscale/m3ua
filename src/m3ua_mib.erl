244a06cd8b737205166b8589c9f977e9ef4586df
Vance Shipley <vances@sigscale.org>
2018-07-17 06:12:12 +0530

    add instrumentation module

---
+++ src/m3ua_mib.erl
@@ -0,0 +1,110 @@
+%%% m3ua_mib.erl
+%%% vim: ts=3
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @copyright 2018 SigScale Global Inc.
+%%% @end
+%%% Licensed under the Apache License, Version 2.0 (the "License");
+%%% you may not use this file except in compliance with the License.
+%%% You may obtain a copy of the License at
+%%%
+%%%     http://www.apache.org/licenses/LICENSE-2.0
+%%%
+%%% Unless required by applicable law or agreed to in writing, software
+%%% distributed under the License is distributed on an "AS IS" BASIS,
+%%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%% See the License for the specific language governing permissions and
+%%% limitations under the License.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @doc This library module implements the SNMP MIB for the
+%%%     {@link //m3ua. m3ua} application.
+%%%
+-module(m3ua_mib).
+-copyright('Copyright (c) 2018 SigScale Global Inc.').
+
+%% export the m3ua_mib public API
+-export([load/0, load/1, unload/0, unload/1]).
+
+%% export the m3ua_mib snmp agent callbacks
+-export([ep_table/3]).
+
+-include("m3ua.hrl").
+
+%%----------------------------------------------------------------------
+%%  The m3ua_mib public API
+%%----------------------------------------------------------------------
+
+-spec load() -> Result
+	when
+		Result :: ok | {error, Reason},
+		Reason :: term().
+%% @doc Loads the SigScale M3UA MIB.
+load() ->
+	case code:priv_dir(ocs) of
+		PrivDir when is_list(PrivDir) ->
+			MibDir = PrivDir ++ "/mibs/",
+			Mibs = [MibDir ++ MIB || MIB <- mibs()],
+			snmpa:load_mibs(Mibs);
+		{error, Reason} ->
+			{error, Reason}
+	end.
+
+-spec load(Agent) -> Result
+	when
+		Agent :: pid() | atom(),
+		Result :: ok | {error, Reason},
+		Reason :: term().
+%% @doc Loads the SigScale M3UA MIB.
+load(Agent) ->
+	case code:priv_dir(ocs) of
+		PrivDir when is_list(PrivDir) ->
+			MibDir = PrivDir ++ "/mibs/",
+			Mibs = [MibDir ++ MIB || MIB <- mibs()],
+			snmpa:load_mibs(Agent, Mibs);
+		{error, Reason} ->
+			{error, Reason}
+	end.
+
+-spec unload() -> Result
+	when
+		Result :: ok | {error, Reason},
+		Reason :: term().
+%% @doc Unloads the SigScale M3UA MIB.
+unload() ->
+	snmpa:unload_mibs(mibs()).
+
+-spec unload(Agent) -> Result
+	when
+		Agent :: pid() | atom(),
+		Result :: ok | {error, Reason},
+		Reason :: term().
+%% @doc Unloads the SigScale M3UA MIB.
+unload(Agent) ->
+	snmpa:unload_mibs(Agent, mibs()).
+
+%%----------------------------------------------------------------------
+%% The m3ua_mib snmp agent callbacks
+%----------------------------------------------------------------------
+
+-spec ep_table(Operation, RowIndex, Columns) -> Result
+	when
+		Operation :: get | get_next,
+		RowIndex :: ObjectId,
+		ObjectId :: [integer()],
+		Columns :: [Column],
+		Column :: integer(),
+		Result :: [Element] | {genErr, Column},
+		Element :: {value, Value} | {ObjectId, Value},
+		Value :: atom() | integer() | string() | [integer()].
+%% @doc Handle SNMP requests for the endpoint table.
+%% @private
+ep_table(get, _RowIndex, Columns) ->
+	{genErr, 0}.
+
+%%----------------------------------------------------------------------
+%% internal functions
+%----------------------------------------------------------------------
+
+%% @hidden
+mibs() ->
+	["SIGSCALE-M3UA-MIB"].
+
