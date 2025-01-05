5287e3bd3e7e68ff1108faf27c52f75258e7c2f1
Vance Shipley <vances@sigscale.org>
2025-01-05 14:00:05 +0800

    correct sctp get status option name

--- src/m3ua_asp_fsm.erl
+++ src/m3ua_asp_fsm.erl
@@ -781,8 +781,7 @@ handle_event({'M-SCTP_STATUS', request, Ref, From}, StateName,
 	{next_state, StateName, StateData};
 handle_event({'M-SCTP_STATUS', request, Ref, From}, StateName,
 		#statedata{socket = Socket, assoc = Assoc} = StateData) ->
-	Options = [{sctp_status, #sctp_status{assoc_id = Assoc}}],
-	case inet:getopts(Socket, Options) of
+	case inet:getopts(Socket, [sctp_status]) of
 		{ok, SCTPStatus} ->
 			{_, Status} = lists:keyfind(sctp_status, 1, SCTPStatus),
 			gen_server:cast(From,
